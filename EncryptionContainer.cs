59d5593836c1d757554a89c0ae00b51e5598a040
AlegDreg <80702802+AlegDreg@users.noreply.github.com>
2022-09-11 15:54:58 +0300

    Add files via upload

---
+++ EncryptionContainer.cs
@@ -0,0 +1,22 @@
+﻿namespace Shifr
+{
+    internal class EncryptionContainer
+    {
+        private IEncrtyption Encrtyption;
+
+        public EncryptionContainer(IEncrtyption encrtyption)
+        {
+            Encrtyption = encrtyption;
+        }
+
+        public string Encrypt(int key, string text)
+        {
+            return Encrtyption.Encrypt(key, text);
+        }
+
+        public string Decrypt(int key, string text)
+        {
+            return Encrtyption?.Decrypt(key, text);
+        }
+    }
+}
